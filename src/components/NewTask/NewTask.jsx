c2132217e51fa1e440489f56242df9d3b6975799
thepierrre <piotr.owczarczyk98@gmail.com>
2023-06-16 10:14:48 +0200

    Fix the + button for adding a task/tag

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -33,6 +33,11 @@ const NewTask = (props) => {
     addNewTagFromMenuHandler,
   } = props;
 
+  const handleAddButton = () => {
+    addNewTaskHandler();
+    addNewTagHandler;
+  };
+
   return (
     <form className="new-task">
       <FormControl>
@@ -83,7 +88,7 @@ const NewTask = (props) => {
           }
         />
       </FormControl>
-      <IconButton onClick={(addNewTaskHandler, addNewTagHandler)}>
+      <IconButton onClick={handleAddButton}>
         <AddCircleIcon fontSize="large" color="info" />
       </IconButton>
     </form>
